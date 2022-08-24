ea7a65bea22fa08e01905a401b767f8b035ff1ad
GustavoBozo <gustavinhobozo@gmail.com>
2022-08-24 10:08:57 -0300

    Componente porta criado

---
+++ components/Porta.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/porta.module.css'
+
+export default function Porta() {
+    return (
+        <div className={styles.porta}>
+            <div className={styles.numero}>3</div>
+        </div>
+    )
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Presente from '../components/Presente'
+import Porta from '../components/Porta'
+
 
 export default function Home() {
   return (
     <div>
-      <Presente />
-      
+      <Porta />
     </div>
   )
 }
