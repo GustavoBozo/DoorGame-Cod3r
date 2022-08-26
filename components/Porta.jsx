96cff9bffb0bbb6e37ed312d5c5b1589028c0fd3
GustavoBozo <gustavinhobozo@gmail.com>
2022-08-26 15:49:53 -0300

    Implementada estrutura da porta e criação de divs

--- components/Porta.jsx
+++ components/Porta.jsx
@@ -2,8 +2,13 @@ import styles from '../styles/porta.module.css'
 
 export default function Porta() {
     return (
-        <div className={styles.porta}>
-            <div className={styles.numero}>3</div>
+        <div className={styles.area}>
+            <div className={styles.batente}>
+                <div className={styles.porta}>
+                    <div className={styles.numero}>3</div>
+                </div>
+            </div>
         </div>
+        
     )
 }
