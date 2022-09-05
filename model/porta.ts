c0e1eb80ac7c757830a0504ef7ed0103829d7f52
GustavoBozo <gustavinhobozo@gmail.com>
2022-09-05 17:08:23 -0300

    Integração inicial com o typescript finalizada

--- model/porta.ts
+++ model/porta.ts
@@ -1,10 +1,10 @@
-export default class Porta {
+export default class PortaModel {
     #numero: number
     #temPresente: boolean
     #selecionada: boolean
     #aberta: boolean
 
-    constructor(numero, temPresente = false, selecionada = false, aberta = false) {
+    constructor(numero: number, temPresente = false, selecionada = false, aberta = false) {
         this.#numero = numero
         this.#temPresente = temPresente
         this.#selecionada = selecionada
@@ -33,7 +33,7 @@ export default class Porta {
         const selecionada = false
         
         //Nova instancia de uma porta
-        return new Porta(this.numero, this.temPresente, selecionada, this.aberta)
+        return new PortaModel(this.numero, this.temPresente, selecionada, this.aberta)
     }
 
     //Metodo para alterar a selecao da porta
@@ -41,15 +41,15 @@ export default class Porta {
         const selecionada = !this.selecionada
         
         //Nova instancia de uma porta
-        return new Porta(this.numero, this.temPresente, selecionada, this.aberta)
+        return new PortaModel(this.numero, this.temPresente, selecionada, this.aberta)
     }
 
     //Metodo para ver se a porta esta aberta
-    alterarSelecao() {
+    abrir() {
         const aberta = true
         
         //Nova instancia de uma porta
-        return new Porta(this.numero, this.temPresente, this.selecionada, aberta)
+        return new PortaModel(this.numero, this.temPresente, this.selecionada, aberta)
     }
 
 
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,14 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { useState } from "react";
 import Presente from '../components/Presente'
 import Porta from '../components/Porta'
-
+import PortaModel from '../model/porta'
 
 export default function Home() {
+  const [p1, setP1] = useState(new PortaModel(1))
+
   return (
     <div style={{display: "flex"}}>
-      <Porta selecionada={true}/>
-      <Porta selecionada={false}/>
+      <Porta porta={p1} />
     </div>
   )
 }
