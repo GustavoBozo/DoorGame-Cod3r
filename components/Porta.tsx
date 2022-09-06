f6aa6a7c3bfb178290dc21134a6c5f7f0b760c7d
GustavoBozo <gustavinhobozo@gmail.com>
2022-09-06 10:44:55 -0300

    Adicionado evento de seleção á porta

--- components/Porta.tsx
+++ components/Porta.tsx
@@ -3,14 +3,18 @@ import PortaModel from '../model/porta'
 
 interface PortaProps {
     value: PortaModel
+    onChange: (novaPorta: PortaModel) => void 
 }
 
 export default function Porta(props: PortaProps) {
     const porta = props.value
     const selected = porta.selecionada ? styles.selecionada : ''
 
+    //Funcoes
+    const alternarSelecao = e => props.onChange(porta.alterarSelecao())
+    
     return (
-        <div className={styles.area}>
+        <div className={styles.area} onClick={alternarSelecao}>
             <div className={`${styles.batente} ${selected}`}>
                 <div className={styles.porta}>
                     <div className={styles.numero}>{ porta.numero }</div>
