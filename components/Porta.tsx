3589e71664ec9cb8ef95dd36605be5243fa5ad23
GustavoBozo <gustavinhobozo@gmail.com>
2022-09-08 10:33:56 -0300

    Adicionado evento de abrir porta

--- components/Porta.tsx
+++ components/Porta.tsx
@@ -8,18 +8,28 @@ interface PortaProps {
 
 export default function Porta(props: PortaProps) {
     const porta = props.value
-    const selected = porta.selecionada ? styles.selecionada : ''
+    const selected = porta.selecionada  && !porta.aberta ? styles.selecionada : ''
 
     //Funcoes
     const alternarSelecao = e => props.onChange(porta.alterarSelecao())
+    const abrir = e => {
+        e.stopPropagation()
+        props.onChange(porta.abrir())
+    }
+
+    function renderizarPota(){
+        return (
+            <div className={styles.porta}>
+                <div className={styles.numero}>{ porta.numero }</div>
+                <div className={styles.macaneta} onClick={abrir}></div>
+            </div>
+        )
+    }
     
     return (
         <div className={styles.area} onClick={alternarSelecao}>
             <div className={`${styles.batente} ${selected}`}>
-                <div className={styles.porta}>
-                    <div className={styles.numero}>{ porta.numero }</div>
-                    <div className={styles.macaneta}></div>
-                </div>
+                {porta.aberta ? false : renderizarPota()}
             </div>
             <div className={styles.chao}></div>
         </div>
