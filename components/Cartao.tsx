fb9aa633be3d2e18327be715e18e8f7abe92689f
GustavoBozo <gustavinhobozo@gmail.com>
2022-09-19 16:14:10 -0300

    Finalizado eventos de porta e formulario de jogo

--- components/Cartao.tsx
+++ components/Cartao.tsx
@@ -1,9 +1,17 @@
 import styles from '../styles/Cartao.module.css'
 
-export default function Cartao(props){
+interface CartaoProps {
+    bgcolor?: string
+    children?: any
+}
+
+export default function Cartao(props: CartaoProps){
     return (
-        <div className={styles.cartao}>
-            
+        <div className={styles.cartao}
+            style={{ 
+                backgroundColor: props.bgcolor ?? "#fff"
+            }}>
+            {props.children}
 
         </div>
     )
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,35 @@
+import Link from 'next/link';
 import Cartao from '../components/Cartao';
+import EntradaNumerica from '../components/EntradaNumerica'
+import styles from '../styles/Form.module.css'
+import { useState } from 'react';
+
 
 export default function Form() {
+  const [qtdePortas, setQtdePortas] = useState(3)
+  const [comPresente, setComPresente] = useState(1)
 
+  
   return (
-    <div>
-      <Cartao />
+    <div className={styles.form}>
+      <div>
+        <Cartao bgcolor="#c0392c">
+          <h1>Monty Hall</h1>
+        </Cartao>
+        <Cartao>
+          <EntradaNumerica text="Qtde Portas?" value={qtdePortas} onChange={novaQtde => setQtdePortas(novaQtde)}/>
+        </Cartao>
+      </div>
+      <div>
+        <Cartao>
+          <EntradaNumerica text="Com Presente" value={comPresente} onChange={comPresente => setComPresente(comPresente)}/>
+        </Cartao>
+        <Cartao bgcolor="#28a085">
+          <Link href={`/jogo/${qtdePortas}/${comPresente}`} >
+            <h1 className={styles.link}>Começar </h1>
+          </Link>
+        </Cartao>
+      </div>
     </div>
   )
 }
